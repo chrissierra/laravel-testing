90e017f4ea16aa619282156dc2283cbdc92594c7
christopher sierra <christopher.sierra@fashionspark.com>
2022-10-18 18:08:52 -0300

    Fix form div, adding some padding and border radius

--- resources/views/components/elfaro-components/contact-form.blade.php
+++ resources/views/components/elfaro-components/contact-form.blade.php
@@ -1,4 +1,4 @@
-<div id="formulario_contacto" class="container-fluid border border-secondary">
+<div id="formulario_contacto" class="container-fluid border border-secondary" style="padding: 3em; border-radius: 1em">
     {{-- 
         nombre_cliente
         mensaje
@@ -7,18 +7,18 @@
     --}}
     <form method="POST" action="{{ route('formulariocontacto.store') }}">
         @csrf
-        <div class="mb-3">
-            <h1 class="text-center">FORMULARIO DE CONTACTO</h1>
+        <div class="mb-4">
+            <h1 class="text-center mb-4">FORMULARIO DE CONTACTO</h1>
             <label for="exampleFormControlInput1" class="form-label">Email address</label>
             <input type="email" name="correo" class="form-control"  placeholder="name@example.com">
         </div>
 
-        <div class="mb-3">
+        <div class="mb-4">
             <label for="exampleFormControlInput1" class="form-label">Nombre</label>
             <input type="text" name="nombre_cliente" class="form-control"  placeholder="Juan perez">
         </div>
 
-        <div class="mb-3">
+        <div class="mb-4">
             <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
             <textarea class="form-control" name="mensaje"  rows="3"></textarea>
         </div>
